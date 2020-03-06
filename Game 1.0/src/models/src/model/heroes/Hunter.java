ecffb97a3581493ec5f0b0b4a34a9665af67a2cd
fad11 <61867975+fad11@users.noreply.github.com>
2020-03-06 12:26:03 +0100

    Ihab files

---
+++ Game 1.0/src/models/src/model/heroes/Hunter.java
@@ -0,0 +1,10 @@
+package game;
+
+public class Hunter extends Hero {
+
+	public Hunter(String n) {
+		super(n);
+		// TODO Auto-generated constructor stub
+		}
+
+}
---
+++ Game 1.0/src/models/src/model/heroes/Paladin.java
@@ -0,0 +1,10 @@
+package game;
+
+public class Paladin  extends Hero{
+
+	public Paladin(String n) {
+		super(n);
+		// TODO Auto-generated constructor stub
+	}
+
+}
