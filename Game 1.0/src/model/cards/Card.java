17f65429c0d54068d38c8974015f9fd0402a1443
alwayskilling1 <abdu.ehab0@gmail.com>
2020-03-09 15:50:16 +0100

    testing

--- Game 1.0/src/model/cards/Card.java
+++ Game 1.0/src/model/cards/Card.java
@@ -10,11 +10,11 @@ abstract public class Card {
 
 	public Card(String name, int manaCost, Rarity rarity) {
 		this.name = name;
-		while(manaCost<=0 || manaCost>10) {
-			if(manaCost<0)
-				manaCost+=10;
+		while (manaCost <= 0 || manaCost > 10) {
+			if (manaCost < 0)
+				manaCost += 1;
 			else
-				manaCost-=10;
+				manaCost -= 1;
 		}
 		this.manaCost = manaCost;
 		this.rarity = rarity;
@@ -33,7 +33,12 @@ abstract public class Card {
 	}
 
 	public void setManaCost(int manaCost) {
-		this.manaCost = manaCost;
+		while (manaCost <= 0 || manaCost > 10) {
+			if (manaCost < 0)
+				manaCost += 1;
+			else
+				manaCost -= 1;
+		}
 	}
 
 	public Rarity getRarity() {
