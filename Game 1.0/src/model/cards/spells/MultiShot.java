060de9b2bb3e16c3abfe602df56c2211f94c98e4
alwayskilling1 <abdu.ehab0@gmail.com>
2020-04-07 10:47:26 +0200

    a7a ya t5een sl7 7gtk

--- Game 1.0/src/model/cards/spells/MultiShot.java
+++ Game 1.0/src/model/cards/spells/MultiShot.java
@@ -17,17 +17,17 @@ public class MultiShot extends Spell implements AOESpell {
 		if (oppField.size() >= 3) {
 
 			int x = (int) (Math.random() * (oppField.size() - 1));
-			oppField.get(x).setCurrentHP(oppField.get(x).getCurrentHP() - 4);
+			oppField.get(x).setCurrentHP(oppField.get(x).getCurrentHP() - 3);
 			;
 
 			int y = (int) (Math.random() * (oppField.size() - 1));
-			oppField.get(y).setCurrentHP(oppField.get(y).getCurrentHP() - 4);
+			oppField.get(y).setCurrentHP(oppField.get(y).getCurrentHP() - 3);
 
 		} else if (oppField.size() == 2) {
-			oppField.get(0).setCurrentHP(oppField.get(0).getCurrentHP() - 4);
-			oppField.get(1).setCurrentHP(oppField.get(1).getCurrentHP() - 4);
+			oppField.get(1).setCurrentHP(oppField.get(0).getCurrentHP() - 3);
+			oppField.get(0).setCurrentHP(oppField.get(1).getCurrentHP() - 3);
 		} else if (oppField.size() == 1) {
-			oppField.get(0).setCurrentHP(oppField.get(0).getCurrentHP() - 4);
+			oppField.get(0).setCurrentHP(oppField.get(0).getCurrentHP() - 3);
 		}
 	}
 }
