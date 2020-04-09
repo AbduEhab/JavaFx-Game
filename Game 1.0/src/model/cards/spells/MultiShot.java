48d04232f17d16611bbaa1d79187c88fa1dc7e8d
alwayskilling1 <abdu.ehab0@gmail.com>
2020-04-09 19:53:04 +0200

    multishot fix

--- Game 1.0/src/model/cards/spells/MultiShot.java
+++ Game 1.0/src/model/cards/spells/MultiShot.java
@@ -17,17 +17,32 @@ public class MultiShot extends Spell implements AOESpell {
 		if (oppField.size() >= 3) {
 
 			int x = (int) (Math.random() * (oppField.size() - 1));
-			oppField.get(x).setCurrentHP(oppField.get(x).getCurrentHP() - 3);
-			;
+			if (!oppField.get(x).isDivine())
+				oppField.get(x).setCurrentHP(oppField.get(x).getCurrentHP() - 3);
+			else
+				oppField.get(x).setDivine(false);
 
 			int y = (int) (Math.random() * (oppField.size() - 1));
-			oppField.get(y).setCurrentHP(oppField.get(y).getCurrentHP() - 3);
+			if (!oppField.get(y).isDivine())
+				oppField.get(y).setCurrentHP(oppField.get(x).getCurrentHP() - 3);
+			else
+				oppField.get(y).setDivine(false);
 
 		} else if (oppField.size() == 2) {
-			oppField.get(1).setCurrentHP(oppField.get(0).getCurrentHP() - 3);
-			oppField.get(0).setCurrentHP(oppField.get(1).getCurrentHP() - 3);
+			if (!oppField.get(1).isDivine())
+				oppField.get(1).setCurrentHP(oppField.get(1).getCurrentHP() - 3);
+			else
+				oppField.get(1).setDivine(false);
+
+			if (!oppField.get(0).isDivine())
+				oppField.get(0).setCurrentHP(oppField.get(0).getCurrentHP() - 3);
+			else
+				oppField.get(0).setDivine(false);
 		} else if (oppField.size() == 1) {
-			oppField.get(0).setCurrentHP(oppField.get(0).getCurrentHP() - 3);
+			if (!oppField.get(0).isDivine())
+				oppField.get(0).setCurrentHP(oppField.get(0).getCurrentHP() - 3);
+			else
+				oppField.get(0).setDivine(false);
 		}
 	}
 }
